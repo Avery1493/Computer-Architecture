d4d33a8c35be9363e0ef4651b6d39f01904ea11a
Avery Quinn <avery.quinn1493@gmail.com>
2020-08-18 14:54:03 -0700

    implement multiply

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,7 +12,6 @@ class CPU:
         self.pc = 0
         self.running = True
 
-    # Memory address register
     def ram_read(self, MAR):
         '''
         take in address and return value
@@ -20,7 +19,6 @@ class CPU:
         MDR = self.ram[MAR]
         return MDR
 
-    # Memory data register
     def ram_write(self, MAR, MDR):
         '''
         take address and write value
@@ -29,31 +27,11 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-        # #program counter
-        # address = 0
-        # # For now, we've just hardcoded a program:
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
-        # Load a program
-        # program counter
         address = 0
         filename = sys.argv[1]
         with open(filename) as f:
             for line in f:
-                # take out comment
-                # remove leading and trailing spaces
+                # remove #, leading and trailing spaces
                 line = line.split("#")[0].strip()
                 # skip empty lines
                 if line == "":
@@ -63,8 +41,6 @@ class CPU:
                     # base 2 (binary)
                     self.ram[address] = int(line, 2)
                     address += 1
-
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
